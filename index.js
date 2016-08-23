0b5dc6ce870a5b531fa11eb11d8c3cb4b00f3633
martin byrne <marb61a@gmail.com>
2016-08-23 21:07:48 +0000

    updated index.js

--- index.js
+++ index.js
@@ -18,3 +18,36 @@ server.route({
     }    
 });
 
+// Register good plugin and then start the server
+server.register({
+    register : require("good"),
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                    log: '*',
+                    response: '*'
+                }]
+            }, {
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+}, (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    // Starting the server
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+
+        console.log('Server running at:', server.info.uri);
+    });
+});
