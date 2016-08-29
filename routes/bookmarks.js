962a88045157cadce64aba329286ba3308d1af3c
martin byrne <marb61a@gmail.com>
2016-08-29 14:21:19 +0000

    added get route to bookmarks

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -15,4 +15,52 @@ exports.register = function (server, options, next){
         delete doc.upvoters;
     };
     
+    server.route({
+        method : 'GET',
+        path : '/bookmarks',
+        handler : function(request, reply){
+            let sort;
+            
+            if(request.query.sort === 'top'){
+                sort ={
+                    $sort : {
+                        upvotes : -1
+                    }
+                };    
+            } else {
+                sort ={
+                    $sort : {
+                        created : -1
+                    }
+                };
+            }
+            
+            db.bookmarks.aggregate({
+                $project : {
+                    title: 1,
+                    url: 1,
+                    created: 1,
+                    upvotes: {
+                        $size: '$upvoters'
+                    }
+                }    
+            }, sort, (err, docs) => {
+                if(err){
+                    throw err;
+                }
+                
+                docs.forEach(_renameAndClearFields);
+                return reply(docs);
+            });
+        },
+        
+        config : {
+            validate : {
+                query : {
+                    sort: Joi.string().valid('top', 'new').default('top')
+                }
+            }
+        }
+    });
+    
 };
